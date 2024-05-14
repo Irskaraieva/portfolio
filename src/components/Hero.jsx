73f8780e81e36920ca0ae46e6b2a7f49c58627b2
Irskaraieva <mishytkina14@gmail.com>
2024-05-14 08:56:19 +0200

    add mobile menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { ComputerCanvas } from './canvas';
+
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative '>
+
+    </section>
   )
 }
 
