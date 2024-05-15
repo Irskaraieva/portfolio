fce26a630ff0858d7a2f53edf51818684ded9482
Irskaraieva <mishytkina14@gmail.com>
2024-05-15 09:37:06 +0200

    3D image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,26 @@
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
-import { ComputerCanvas } from './canvas';
+import { ComputersCanvas } from './canvas';
 
 import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='relative '>
-
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justyfy-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className='text-[#915eff]'>Iryna</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop user-friendly responsive pages.<br />
+            I prefer custom styles, but I also use frameworks like Bulma, Tailwind.
+          </p>
+        </div>        
+      </div>
+      <ComputersCanvas />
     </section>
   )
 }
